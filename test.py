f874a9bdd979cf76d1dc2c9fca7c13cd3ea2f9a7
YAQ <yaqube147@gmail.com>
2024-02-20 00:39:04 +0800

    This is the start havent done anything just the framework

---
+++ test.py
@@ -0,0 +1,11 @@
+#This is a program that tracks how long I spend on nvim & adds it to my gmail so that I can see how long i spend programming 
+
+#THIS IS THE LINUX VERSION 
+
+#This secltion is the part that checks for the distro:
+#It will check for the distro of which package is used; rpm, flatpak, etc.
+
+
+#This checks for whether a gmail account has been added to the device
+
+#This is the rest i have not labelled yet
